ebd273802caca4b9ff82d64c35f44d6ea82a5dec
Simon McVittie <smcv@debian.org>
2008-12-17 23:35:41 +0000

    Imported Upstream version 0.5.10

---
+++ check-coding-style.mk
@@ -0,0 +1,15 @@
+check-local::
+	@fail=0; \
+	if test -n "$(check_misc_sources)"; then \
+		top_srcdir=$(top_srcdir) sh $(top_srcdir)/check-whitespace.sh \
+			$(check_misc_sources) || fail=1; \
+	fi; \
+	if test -n "$(check_c_sources)"; then \
+		top_srcdir=$(top_srcdir) sh $(top_srcdir)/check-c-style.sh \
+			$(check_c_sources) || fail=1; \
+	fi;\
+	if test yes = "$(ENABLE_CODING_STYLE_CHECKS)"; then \
+		exit "$$fail";\
+	else \
+		exit 0;\
+	fi
---
+++ telepathy-glib/interfaces.h
@@ -0,0 +1,6 @@
+#ifndef __TP_INTERFACES_H__
+#define __TP_INTERFACES_H__
+
+#include <telepathy-glib/_gen/telepathy-interfaces.h>
+
+#endif
